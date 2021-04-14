5fa0362e270c561f2b30faa564d0785aeb8c1578
Cindy <ziishe1995@gmail.com>
2021-04-14 15:48:36 +0800

    addLangMap

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,8 @@
 
 <script lang="ts">
 import leaflet from 'leaflet'
-import { ref, onMounted, toRefs, watch } from 'vue'
+import { ref, onMounted, toRefs, watch, computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
   name: 'Map',
@@ -15,6 +16,10 @@ export default {
     }
   },
   setup(props: any, { emit }: any) {
+    //init
+    const store = useStore()
+    //state
+    const lang: any = computed(() => store.state.app.lang)
     const Map = ref({})
     const { mapData } = toRefs(props)
 
@@ -23,27 +28,33 @@ export default {
         data = {
           lat: mapData.value[0].lat,
           lng: mapData.value[0].lng,
-          sna: mapData.value[0].sna
+          sna:
+            lang.value === 'ch' ? mapData.value[0].sna : mapData.value[0].snaen,
+          sbi: mapData.value[0].sbi,
+          tot: mapData.value[0].tot
         }
       }
+      const map: any = Map.value
+      map.flyTo(new leaflet.LatLng(data.lat, data.lng), 15)
       leaflet
         .marker([data.lat, data.lng])
         .addTo(Map.value)
-        .bindPopup(data.sna)
+        .bindPopup(
+          `${data.sna}<br>
+        <span style="color: #eb9549">
+        ${lang.value === 'ch' ? '可借車輛' : 'Can be by vehicle'}:${data.sbi}
+        </span><br>
+        <span style="color: #f6da77">
+        ${lang.value === 'ch' ? '可停空位' : 'Can be stopped'}:${data.tot}
+        </span>
+        `
+        )
         .openPopup()
     }
     const getMap = () => {
-      let fistVal = {
+      const fistVal = {
         lat: 25.041956,
-        lng: 121.508791,
-        sna: 'A pretty CSS3 popup.<br> Easily customizable.'
-      }
-      if (mapData.value.length) {
-        fistVal = {
-          lat: mapData.value[0].lat,
-          lng: mapData.value[0].lng,
-          sna: mapData.value[0].sna
-        }
+        lng: 121.508791
       }
       //
       Map.value = leaflet.map('map', {
@@ -67,4 +78,8 @@ export default {
   }
 }
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.test {
+  color: #409eff;
+}
+</style>
